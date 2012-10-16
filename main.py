6001b60a9913e3824e1a4fb24c53822fa56f69e6
Igor de Oliveira Sa <igordeoliveirasa@gmail.com>
2012-10-15 22:53:42 -0300

    Calculating the 15% to get out

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-__author__ = 'igor'
+import sys
 
 
-
-
-print "alow!"
+while True:
+    max_value = float(raw_input("Enter the max stock value: "))
+    print "You must sell when the value reaches: %.2f" %  (max_value - (max_value * 0.15))
